aedcc998ed0297291b5131fad507f8b895734d73
POEMisaMisa <eon40qb8mk@gmail.com>
2016-09-23 22:40:38 +0700

    Public release

--- poetrade_ProfileLink_Blacklist_Notes.user.js
+++ poetrade_ProfileLink_Blacklist_Notes.user.js
@@ -4,40 +4,42 @@
 // @include     http://poe.trade/*
 // @include     https://poe.trade/*
 // @version     1.2
-// @author      MisaMisa, kylegetsspam, pollyzoid
+// @author      MisaMisa, kylegetsspam, KHS_aAa, pollyzoid
 // @run-at      document-end
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // ==/UserScript==
 
-var isAccountBlacklisted = function(account_name) {
-    return GM_getValue("isBlocked_" + account_name, "false") === "true";
+var forum_thread_id = 1741446;
+
+var isProfileBlacklisted = function(profile_name) {
+    return GM_getValue("isBlocked_" + profile_name, "false") === "true";
 };
 
-var blacklistAccount = function(account_name) {
-    GM_setValue("isBlocked_" + account_name, "true");
+var blacklistProfile = function(profile_name) {
+    GM_setValue("isBlocked_" + profile_name, "true");
 };
 
-var unblacklistAccount = function(account_name) {
-    GM_deleteValue("isBlocked_" + account_name);
+var unblacklistProfile = function(profile_name) {
+    GM_deleteValue("isBlocked_" + profile_name);
 };
 
-var getAccountCustomNote = function(account_name) {
-    return GM_getValue("customNote_" + account_name, "").trim();
+var getProfileCustomNote = function(profile_name) {
+    return GM_getValue("customNote_" + profile_name, "").trim();
 };
 
-var setAccountCustomNote = function(account_name, note) {
-    GM_setValue("customNote_" + account_name, note);
+var setProfileCustomNote = function(profile_name, note) {
+    GM_setValue("customNote_" + profile_name, note);
 };
 
-var deleteAccountCustomNote = function(account_name) {
-    GM_deleteValue("customNote_" + account_name);
+var deleteProfileCustomNote = function(profile_name) {
+    GM_deleteValue("customNote_" + profile_name);
 };
 
 var updateTradeItemNodes = function(results) {
     Array.prototype.forEach.call(results.querySelectorAll('.item'), function(item) {
-        var account_name = item.getAttribute('data-seller');
+        var profile_name = item.getAttribute('data-seller');
         var ign_name = item.getAttribute('data-ign');
 
         // Detect if russian
@@ -49,10 +51,10 @@ var updateTradeItemNodes = function(results) {
             }
         }
 
-        var is_blocked = isAccountBlacklisted(account_name);
-        var custom_note = getAccountCustomNote(account_name);
+        var is_blocked = isProfileBlacklisted(profile_name);
+        var custom_note = getProfileCustomNote(profile_name);
 
-        var temp_string = '<ul class="proplist">' + (is_russian ? '<li><img src="http://i.imgur.com/tRDP5C9.png" alt="RU" title="This player is russian"></li>' : '') + '<li><a href="https://www.pathofexile.com/account/view-profile/' + account_name + '" target="_blank" title="Click to open profile link in new tab">View profile</a></li><li><a href="#" onclick="return false" class="block-btn">' + (is_blocked ? 'Unblock' : 'Block') + '</a></li><li><a href="#" onclick="return false" class="note-btn" title="Click to edit">' + (custom_note === "" ? 'Edit note' : custom_note) + '</a></li></ul>';
+        var temp_string = '<ul class="proplist">' + (is_russian ? '<li><img src="http://i.imgur.com/tRDP5C9.png" alt="RU" title="This player is russian"></li>' : '') + '<li><a href="https://www.pathofexile.com/account/view-profile/' + profile_name + '" target="_blank" title="Click to open profile link in new tab">View profile</a></li><li><a href="#" onclick="return false" class="block-btn">' + (is_blocked ? 'Unblock' : 'Block') + '</a></li><li><a href="#" onclick="return false" class="note-btn" title="Click to edit">' + (custom_note === "" ? 'Edit note' : custom_note) + '</a></li></ul>';
 
         // Buttons
         item.querySelector('.bottom-row .third-cell').insertAdjacentHTML('beforeend', temp_string);
@@ -88,15 +90,15 @@ if (live_items) {
     observer.observe(live_items, { childList: true });
 }
 
-var updateBlackListStatus = function(results, target_account_name) {
+var updateBlackListStatus = function(results, target_profile_name) {
     Array.prototype.forEach.call(results.querySelectorAll('.item'), function(item) {
-        var account_name = item.getAttribute('data-seller');
+        var profile_name = item.getAttribute('data-seller');
 
-        if (account_name !== target_account_name) {
+        if (profile_name !== target_profile_name) {
             return;
         }
 
-        var is_blocked = isAccountBlacklisted(account_name);
+        var is_blocked = isProfileBlacklisted(profile_name);
 
         // Adjust opacity
         if (is_blocked) {
@@ -113,70 +115,151 @@ var updateBlackListStatus = function(results, target_account_name) {
     });
 };
 
-var updateCustomNoteStatus = function(results, target_account_name) {
+var updateCustomNoteStatus = function(results, target_profile_name) {
     Array.prototype.forEach.call(results.querySelectorAll('.item'), function(item) {
-        var account_name = item.getAttribute('data-seller');
+        var profile_name = item.getAttribute('data-seller');
 
-        if (account_name !== target_account_name) {
+        if (profile_name !== target_profile_name) {
             return;
         }
 
         // Change button text
         var note_button_element = item.querySelector('.note-btn');
         if (note_button_element !== null) {
-            var custom_note = getAccountCustomNote(account_name);
+            var custom_note = getProfileCustomNote(profile_name);
 
             $(note_button_element).text(custom_note === "" ? "Edit note" : custom_note);
         }
     });
 };
 
-var updateAllElements = function(target_function, target_account_name) {
+var updateAllElements = function(target_function, target_profile_name) {
     // Static items
     var all_elements_target = document.getElementsByClassName('search-results-block')[0];
     if (all_elements_target) {
-        target_function(all_elements_target, target_account_name);
+        target_function(all_elements_target, target_profile_name);
     }
 
     // Live items
     var live_items_target = document.getElementById('items');
     if (live_items_target) {
-        target_function(live_items_target, target_account_name);
+        target_function(live_items_target, target_profile_name);
     }
 };
 
 // Block button handler
 $(document).on('click', '.block-btn', function() {
     var item = $(this).parents(".item");
-    var account_name = item.data("seller");
+    var profile_name = item.data("seller");
 
-    if (isAccountBlacklisted(account_name)) {
-        unblacklistAccount(account_name);
+    if (isProfileBlacklisted(profile_name)) {
+        unblacklistProfile(profile_name);
     } else {
-        blacklistAccount(account_name);
+        blacklistProfile(profile_name);
     }
 
     // Update all elements
-    updateAllElements(updateBlackListStatus, account_name);
+    updateAllElements(updateBlackListStatus, profile_name);
 });
 
 // Note button handler
 $(document).on('click', '.note-btn', function() {
     var item = $(this).parents(".item");
-    var account_name = item.data("seller");
+    var profile_name = item.data("seller");
 
-    var note = prompt('Enter custom note:', getAccountCustomNote(account_name));
+    var note = prompt('Enter custom note:', getProfileCustomNote(profile_name));
 
     if (note !== null) {
         note = note.trim();
 
         if (note === "") {
-            deleteAccountCustomNote(account_name);
+            deleteProfileCustomNote(profile_name);
         } else {
-            setAccountCustomNote(account_name, note);
+            setProfileCustomNote(profile_name, note);
         }
 
         // Update all elements
-        updateAllElements(updateCustomNoteStatus, account_name);
+        updateAllElements(updateCustomNoteStatus, profile_name);
+    }
+});
+
+// Global buttons
+(function() {
+    var temp_string = '<table class="search-results"><tbody><tr><td><ul class="proplist"><li><a href="#" onclick="return false" class="global-block-btn">Block profile</a></li><li><a href="#" onclick="return false" class="global-unblock-btn">Unblock profile</a></li><li><a href="#" onclick="return false" class="global-note-btn">Edit profile note</a></li><li><a href="#" onclick="return false" class="global-info-btn">View stored profile info</a></li><li><a href="https://www.pathofexile.com/forum/view-thread/' + forum_thread_id + '" target="_blank">Script forum thread</a></li></ul></td></tr></tbody></table>';
+
+    document.querySelector('.protip').insertAdjacentHTML('afterend', temp_string);
+})();
+
+// Global block button handler
+$(document).on('click', '.global-block-btn', function() {
+    var profile_name = prompt('Enter profile name you want to block:', '');
+
+    if (profile_name !== null) {
+        profile_name = profile_name.trim();
+
+        if (profile_name !== "") {
+            blacklistProfile(profile_name);
+
+            // Update all elements
+            updateAllElements(updateBlackListStatus, profile_name);
+        }
+    }
+});
+
+// Global unblock button handler
+$(document).on('click', '.global-unblock-btn', function() {
+    var profile_name = prompt('Enter profile name you want to unblock:', '');
+
+    if (profile_name !== null) {
+        profile_name = profile_name.trim();
+
+        if (profile_name !== "") {
+            unblacklistProfile(profile_name);
+
+            // Update all elements
+            updateAllElements(updateBlackListStatus, profile_name);
+        }
+    }
+});
+
+// Global edit note button handler
+$(document).on('click', '.global-note-btn', function() {
+    var profile_name = prompt('Enter profile name:', '');
+
+    if (profile_name !== null) {
+        profile_name = profile_name.trim();
+
+        if (profile_name !== "") {
+            var note = prompt('Enter custom note for ' + profile_name + ':', getProfileCustomNote(profile_name));
+
+            if (note !== null) {
+                note = note.trim();
+
+                if (note === "") {
+                    deleteProfileCustomNote(profile_name);
+                } else {
+                    setProfileCustomNote(profile_name, note);
+                }
+
+                // Update all elements
+                updateAllElements(updateCustomNoteStatus, profile_name);
+            }
+        }
+    }
+});
+
+// Global view stored info button handler
+$(document).on('click', '.global-info-btn', function() {
+    var profile_name = prompt('Enter profile name:', '');
+
+    if (profile_name !== null) {
+        profile_name = profile_name.trim();
+
+        if (profile_name !== "") {
+            var is_blocked = isProfileBlacklisted(profile_name);
+            var note = getProfileCustomNote(profile_name);
+
+            alert('Profile ' + profile_name + ' is ' + (is_blocked ? '' : 'not ') + 'blocked. Custom note' + (note === "" ? ' not set.' : ': ' + note));
+        }
     }
 });
